eacd0e3ef87fd82fb523b78df09ea78fc3e4b7db
Alec Lee <alec.lee15@gmail.com>
2023-06-28 16:48:49 -0700

    add dummy api route

---
+++ app/api/create/route.ts
@@ -0,0 +1,6 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  return NextResponse.json({ hello: "world" });
+}
