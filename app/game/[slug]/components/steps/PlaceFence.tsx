8de225e9e9d4226e30ea5da4fe9e84b15dd84922
Alec Lee <alec.lee15@gmail.com>
2023-07-06 01:02:03 -0700

    rip

--- app/game/[slug]/components/steps/PlaceFence.tsx
+++ app/game/[slug]/components/steps/PlaceFence.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { submitTurn } from "../../GameStateMachineActions";
-import { useGameStateMachineDispatch } from "../../GameStateMachineContext";
 import { CancelButton } from "./CancelButton";
 import { FenceStep } from "@/app/util/GameStateMachineTypes";
 import { GameState } from "@/app/util/GameTypes";
@@ -13,18 +11,6 @@ interface PlaceFenceProps {
 }
 
 export function PlaceFence({ step, gameState, playerId }: PlaceFenceProps) {
-  const dispatch = useGameStateMachineDispatch();
-
-  const onSkip = React.useCallback(async () => {
-    dispatch(
-      await submitTurn(gameState, playerId, {
-        type: "standard",
-        house: step.house,
-        housePosition: step.position,
-      })
-    );
-  }, [dispatch, step, gameState, playerId]);
-
   return (
     <div className="space-x-2">
       <SkipButton step={step} gameState={gameState} playerId={playerId} text="Skip Fence" />
