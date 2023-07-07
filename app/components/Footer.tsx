143e93e9a6bf80ca3e3830eab1772aff9e9d64d4
Alec Lee <alec.lee15@gmail.com>
2023-07-07 13:31:14 -0700

    no periods!

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function Footer() {
   return (
     <div className="fixed w-full text-center py-4 bottom-0 bg-gray-500 text-white text-lg flex justify-center items-center">
-      <p>Made by Alec and Liz.</p>
+      <p>Made by Alec and Liz</p>
       <a href="https://github.com/al63/welcome-game" target="_blank" rel="noreferrer">
         <Image className="ml-2" height="22" width="22" src="/github-mark-white.svg" alt="Github source" />
       </a>
