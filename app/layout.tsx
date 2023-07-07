8e7657e497b551405fe56179d7a079198d81080f
Alec Lee <alec.lee15@gmail.com>
2023-07-07 14:28:14 -0700

    meetadeeta

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,18 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const title = "Welcome To Your Perfect Home";
+const description = "Play Welcome To Your Perfect Home online";
+
 export const metadata = {
-  title: "Welcome To Your Perfect Home",
-  description: "Play Welcome To Your Perfect Home online",
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    url: "https://welcome-to-your-perfect-home.vercel.app",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
