bd6939eb51e6234d044da20de72c132bb11683ac
Alec Lee <alec.lee15@gmail.com>
2023-07-08 14:26:40 -0700

    display upcoming when prompting shuffle

--- app/game/[slug]/components/steps/ReshuffleOptions.tsx
+++ app/game/[slug]/components/steps/ReshuffleOptions.tsx
@@ -3,6 +3,7 @@ import { submitTurn } from "../../GameStateMachineActions";
 import { useGameStateMachineDispatch } from "../../GameStateMachineContext";
 import { PromptReshuffleStep } from "@/app/util/GameStateMachineTypes";
 import { GameState } from "@/app/util/GameTypes";
+import { modifierDisplayName } from "./Card";
 
 interface ReshuffleProps {
   gameState: GameState;
@@ -11,6 +12,7 @@ interface ReshuffleProps {
 }
 export function ReshuffleOptions({ gameState, step, playerId }: ReshuffleProps) {
   const dispatch = useGameStateMachineDispatch();
+  const upcoming = gameState.revealedCardValues.map((card) => modifierDisplayName(card.backingType));
 
   const onClick = React.useCallback(
     async (reshuffle: boolean) => {
@@ -20,13 +22,16 @@ export function ReshuffleOptions({ gameState, step, playerId }: ReshuffleProps) 
   );
 
   return (
-    <div className="flex gap-4">
-      <button className="px-8 py-2 rounded-full bg-yellow-200 hover:bg-yellow-300" onClick={() => onClick(true)}>
-        Yes
-      </button>
-      <button className="px-8 py-2 rounded-full bg-yellow-200 hover:bg-yellow-300" onClick={() => onClick(false)}>
-        No
-      </button>
+    <div className="flex flex-col">
+      <p className="my-2 italic text-sm">{`Upcoming: ${upcoming.join(", ")}`}</p>
+      <div className="flex gap-4">
+        <button className="px-8 py-2 rounded-full bg-yellow-200 hover:bg-yellow-300" onClick={() => onClick(true)}>
+          Yes
+        </button>
+        <button className="px-8 py-2 rounded-full bg-yellow-200 hover:bg-yellow-300" onClick={() => onClick(false)}>
+          No
+        </button>
+      </div>
     </div>
   );
 }
